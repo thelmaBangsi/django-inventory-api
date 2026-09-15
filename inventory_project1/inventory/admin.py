fc9e15cac64e90c9743f8920246eede0efd88882
thelmaBangsi <bangsithelma@gmail.com>
2026-09-15 12:52:16 +0200

    chore: remove duplicate nested project directory and stray url files

--- inventory_project1/inventory/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Item
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'quantity', 'price', 'total_price', 'created_at')
-    search_fields = ('name',)
